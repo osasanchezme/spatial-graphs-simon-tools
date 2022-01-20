967215c193278122568b83907c220837a8c07c01
Oscar Sanchez <osasanchezme@unal.edu.co>
2022-01-20 11:09:00 -0500

    Basic 3d plot ready

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,13 @@ module.exports = {
     entry: ['./app/main.js'],
     output: {
         filename: 'bundle.js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'ify-loader'
+            }
+        ]
+    },
 };
